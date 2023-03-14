bfb0c3b09261b590e11f6e737d54333d5427a218
Avishka Lakshan <avishka.2019193@iitac.lk>
2023-03-14 22:24:19 +0800

    Initial Commit

---
+++ Test_practice/Test_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_practiceApp.swift
+//  Test_practice
+//
+//  Created by user235114 on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
